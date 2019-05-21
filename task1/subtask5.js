58c168ccabe53623c50f007dbd2eb675c0124a0f
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-05-21 14:06:32 +0300

    feat: modify array of objects

---
+++ task1/subtask5.js
@@ -0,0 +1,85 @@
+var movieLists = [
+{
+	name: "Instant Queue",
+	videos : [{
+		"id": 70111470,
+		"title": "Die Hard",
+		"boxarts": [{
+            width: 150,
+            height: 200,
+            url: "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg"
+        }, {
+            width: 200,
+            height: 200,
+            url: "http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" 
+        }],
+		"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+		"rating": 4.0,
+		"bookmark": []
+    }, {
+		"id": 654356453,
+		"title": "Bad Boys",
+		"boxarts": [{
+            width: 200,
+            height: 200,
+            url: "http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg"
+        }, {
+            width: 150,
+            height: 200,
+            url: "http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg"
+        }],
+		"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+		"rating": 5.0,
+		"bookmark": [{ id: 432534, time: 65876586 }]
+    }]
+},
+{
+	name: "New Releases",
+	videos: [{
+		"id": 65432445,
+		"title": "The Chamber",
+		"boxarts": [{
+            width: 150,
+            height: 200,
+            url: "http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg"
+        }, {
+            width: 200,
+            height: 200,
+            url: "http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" 
+        }],
+		"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+		"rating": 4.0,
+		"bookmark": []
+	}, {
+		"id": 675465,
+		"title": "Fracture",
+		"boxarts": [{
+            width: 200,
+            height: 200,
+            url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg"
+        }, {
+            width: 150,
+            height: 200,
+            url: "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg"
+        }, {
+            width: 300,
+            height: 200,
+            url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg"
+        }],
+		"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+		"rating": 5.0,
+		"bookmark": [{ id: 432534, time: 65876586 }]
+	}]
+}];
+
+const arrays = movieLists.map(object => 
+    object.videos.map(video => ({
+        id: video.id,
+        title: video.title,
+        boxart: video.boxarts.filter(boxart => boxart.width === 150)[0].url,
+
+    }))
+);
+
+const newArray = arrays[0].concat(arrays[1]);
+console.log(newArray);
