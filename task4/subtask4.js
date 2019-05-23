ce88c08288e66d8fc0635b9665ec5b9e41a95aa5
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-05-23 13:34:46 +0300

    feat: generate asynchronous functions

---
+++ task4/subtask4.js
@@ -0,0 +1,34 @@
+const generateNumber = (min, max) => {
+    return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+};
+
+const generateFunction = (index, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Function #' + index);
+            console.log('Executed in ' + delay + ' seconds');
+            resolve();
+        }, delay * 1000);
+    });
+};
+
+const work = () => {
+    const numberOfFunctions = generateNumber(1, 10);
+    const promises = [];
+
+    const startTime = new Date().getTime();
+
+    for (let i = 1; i < numberOfFunctions; i++) {
+        let time = generateNumber(1, 5);
+        promises.push(generateFunction(i, time));
+    }
+
+    Promise.all(promises)
+    .then(() => {
+        const endTime = new Date().getTime();
+        const seconds = Math.round((endTime - startTime) / 1000);
+        console.log('Congrats! Work is done! Whole process took ' + seconds + ' seconds');
+    });
+};
+
+work();
