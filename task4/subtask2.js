46616cb438050a03e195b721f33a38ddfb892233
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-05-28 11:28:02 +0300

    fix: fix arithmetic operations

--- task4/subtask2.js
+++ task4/subtask2.js
@@ -2,11 +2,11 @@ new Promise(resolve => {
     setTimeout(() => resolve(10), 3000);
   }).then((result) => {
       console.log(result);
-      return result * 2;
+      return result + 2;
   }).then((result) => {
     console.log(result);
     return new Promise(resolve => {
-        setTimeout(() => resolve(result * 2), 2000);
+        setTimeout(() => resolve(result + 2), 2000);
     });
   }).then((result) => {
     console.log(result);
