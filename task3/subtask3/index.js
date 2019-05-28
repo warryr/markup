156570afd354b1a3488350c8777cbba7f0a579ac
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-05-28 12:51:36 +0300

    refactor: move function declaration

--- task3/subtask3/index.js
+++ task3/subtask3/index.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
     const secondsButton = document.getElementById('button');
     let interval = null;
 
+    const generateSeconds = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
+
     secondsButton.addEventListener('click', (e) => {
         if (interval === null) {
             const seconds = generateSeconds(1, 4);
@@ -11,6 +13,4 @@ document.addEventListener('DOMContentLoaded', (e) => {
             interval = null;
         }
     });
-
-    const generateSeconds = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
 });
